aa571e5699e69e71755405c5b99aa47d5077eed2
oussama <elabedouss@gmail.com>
2022-07-08 15:10:33 +0100

    Initial project architecture

---
+++ src/main/java/com/brewery/wholesale/main/BreweryWholesaleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.brewery.wholesale.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryWholesaleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryWholesaleManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brewery/wholesale/management/BreweryWholesaleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brewery.wholesale.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryWholesaleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
